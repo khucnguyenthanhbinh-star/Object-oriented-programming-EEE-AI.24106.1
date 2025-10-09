dde2ee9cb00116a84b1356aa8334e003c247fea8
vututhan <24101325@st.phenikaa-uni.edu.vn>
2025-10-09 15:21:00 +0700

    Delete random_list.py

--- random_list.py
+++
@@ -1,9 +0,0 @@
-class RandomList:
-    def get_random_element(self):
-        if not self:
-            return None
-        index = random.randrange(len(self))
-        return self.pop(index)
-
-
-
