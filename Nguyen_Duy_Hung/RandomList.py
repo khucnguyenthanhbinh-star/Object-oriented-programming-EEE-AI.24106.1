3a8f6d610b0b9e89525b657abd5725e152c8cd33
NguyenDuyHung-24103225 <24103225@st.phenikaa-uni.edu.vn>
2025-10-09 15:02:17 +0700

    Delete Nguyen_Duy_Hung directory

--- Nguyen_Duy_Hung/RandomList.py
+++
@@ -1,8 +0,0 @@
-import random
-class RandomList(list):
-    def get_random_element(self):
-        if not self:
-            return None
-        element = random.choice(self)
-        self.remove(element)
-        return element
--- Nguyen_Duy_Hung/project2/teacher.py
+++
@@ -1,5 +0,0 @@
-from person import Person
-from employee import Employee
-class Teacher(Person,Employee):
-    def teach(self):
-        return "teaching..."
