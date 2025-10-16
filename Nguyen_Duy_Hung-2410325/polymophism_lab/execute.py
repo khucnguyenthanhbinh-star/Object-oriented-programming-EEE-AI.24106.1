8c7902dd3f1f103a6870156f818420ef0bb8a00f
NguyenDuyHung-24103225 <24103225@st.phenikaa-uni.edu.vn>
2025-10-16 14:15:01 +0700

    Delete Nguyen_Duy_Hung-2410325 directory

--- Nguyen_Duy_Hung-2410325/polymophism_lab/execute.py
+++
@@ -1,9 +0,0 @@
-
-def say_hello(name,my_name):
-    print(f"Hello, {name}, my name is {my_name}")
-def say_bye(name):
-    print(f"Bye,{name}")
-
-
-execute(say_hello, "Peter", "George")
-execute(say_bye, "Peter")
